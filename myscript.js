85f576e691d770d8a0fd9b26db0f193d634f0fea
Varun Kumar <heysadboy@gmail.com>
2021-06-06 23:36:58 +0900

    practice!

--- myscript.js
+++ myscript.js
@@ -1,3 +1,24 @@
-const hello = () => {
-    console.log("hello");
-}
+const timeout_test = () => {
+    document.getElementById("choice").innerHTML = "Timeout Test";
+    document.getElementById("action").innerHTML = "<b>Hello!</b> will appear after 3 seconds!";
+    const text = "Hello!";
+    setTimeout(set_text, 3000, text);
+};
+
+const callback_test = () => {
+    document.getElementById("choice").innerHTML = "Callback Test";
+    document.getElementById("action").innerHTML = "Name will appear after 3 seconds!";
+    let name = prompt("Enter you name");
+    const print_name = (text) => {
+        document.getElementById("output").innerHTML = text;
+    }
+    setTimeout(call_back, 3000, print_name, name)
+};
+
+const set_text = (text) => {
+    document.getElementById("output").innerHTML = text;
+};
+
+const call_back = (print_name, text) => {
+    print_name(text);
+};
