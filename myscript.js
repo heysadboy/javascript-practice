b612ecf23997636bc953fb10bc68a7bf6edda82d
Varun Kumar <heysadboy@gmail.com>
2021-06-07 19:56:14 +0900

    adding test for setinterval and async await

--- myscript.js
+++ myscript.js
@@ -15,8 +15,64 @@ const callback_test = () => {
     setTimeout(call_back, 3000, print_name, name)
 };
 
+const promise_test = () => {
+    document.getElementById("choice").innerHTML = "Promise Test";
+    document.getElementById("action").innerHTML = "Testing promise";
+
+    const promise = new Promise((resolve, reject) => {
+        resolve("Promise is resolved!")
+        setTimeout(() => {
+            reject("Error after 3 seconds")
+        }, 3000);
+    });
+
+    promise.then(
+        text => {
+            set_text(text);
+        },
+        error => {
+            set_text(error);
+        }
+    ).catch(
+        error => {
+            set_text(error);
+        }
+    )
+};
+
+const async_promise = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("Async Promise test") }, 4000);
+});
+
+const async_await_test = async () => {
+    document.getElementById("choice").innerHTML = "Async/Await Test";
+    document.getElementById("action").innerHTML = "Text will appear after 4 seconds!";
+    let result = await async_promise;
+    set_text(result);
+};
+
+const setinterval_test = () => {
+    document.getElementById("choice").innerHTML = "Set Interval Test";
+    document.getElementById("action").innerHTML = "Countdown from 5 to 1";
+
+    let count = 5;
+    const countdown = () => {
+        set_text(count);
+        count -= 1;
+        if (count === -1) {
+            set_text("Hurray!")
+            clearInterval(interval);
+        }
+    };
+    let interval = setInterval(countdown, 1000);
+};
+
 const set_text = (text) => {
     document.getElementById("output").innerHTML = text;
+};
+
+const set_error = () => {
+    document.getElementById("output").innerHTML = "Error!";
 };
 
 const call_back = (print_name, text) => {
