f6cf5bdc2fbc749deb64bfc723d8397de8187878
Varun Kumar <heysadboy@gmail.com>
2021-07-21 16:30:20 +0900

    call function practice

---
+++ functions/call-function.js
@@ -0,0 +1,25 @@
+const animal = {
+     name: 'emily',
+     type: 'poodle',
+     info: function() {
+        console.log(`${this.name} is a ${this.type}`)
+    }
+}
+
+const dog = {
+    type: 'bulldog',
+    name: 'gustav'
+}
+
+const canary = {
+    type: 'canary',
+    name: 'tweety'
+}
+
+function bird (sound) {
+    console.log(`${this.name} is a ${this.type} and he ${sound}`);
+}
+
+animal.info();
+animal.info.call(dog);
+bird.call(canary, 'chirps');
